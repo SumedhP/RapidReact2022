a9880be5793fd9fbd7a55ee94c77f1866159d9f1
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-06-05 11:54:51 -0700

    Remy adding in some noodles

--- src/main/java/frc/team2412/robot/subsystem/WpiDriveSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystem/WpiDriveSubsystem.java
@@ -1,17 +1,14 @@
 package frc.team2412.robot.subsystem;
 
-import static frc.team2412.robot.Hardware.BACK_LEFT_CONFIG;
-import static frc.team2412.robot.Hardware.BACK_RIGHT_CONFIG;
-import static frc.team2412.robot.Hardware.DRIVETRAIN_INTAKE_CAN_BUS_NAME;
-import static frc.team2412.robot.Hardware.FRONT_LEFT_CONFIG;
-import static frc.team2412.robot.Hardware.FRONT_RIGHT_CONFIG;
-import static frc.team2412.robot.Hardware.GYRO_PORT;
+import static frc.team2412.robot.Hardware.*;
 
 import org.frcteam2910.common.robot.drivers.PigeonTwo;
-import org.frcteam2910.common.robot.drivers.PigeonTwo.Axis;
 
 import com.swervedrivespecialties.swervelib.SwerveModule;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -19,6 +16,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
@@ -29,7 +27,7 @@ public class WpiDriveSubsystem extends SubsystemBase {
     private static final double WHEELBASE = TRACKWIDTH;
     private static final double maxVelocityMetersPerSecond = 1;
 
-    private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+    private static final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
             new Translation2d(TRACKWIDTH / 2.0, WHEELBASE / 2.0), // front left
             new Translation2d(TRACKWIDTH / 2.0, -WHEELBASE / 2.0), // front right
             new Translation2d(-TRACKWIDTH / 2.0, WHEELBASE / 2.0), // back left
@@ -78,12 +76,52 @@ public class WpiDriveSubsystem extends SubsystemBase {
     }
 
     public Rotation2d getHeading() {
-        return new Rotation2d(gyro.getAxis(Axis.YAW));
+        return new Rotation2d(gyro.getAdjustmentAngle().toRadians());
     }
 
-    public SwerveControllerCommand generateTrajectoryCommand(Trajectory trajectory) {
-        return new SwerveControllerCommand(trajectory, () -> odometry.getPoseMeters(), kinematics, null, null, null,
-                null, null, null);
+    public void resetGyro() {
+        gyro.setAdjustmentAngle(gyro.getUnadjustedAngle());
     }
 
+    public Pose2d getPose() {
+        return odometry.getPoseMeters();
+    }
+
+    public void setPose(Pose2d pose) {
+        odometry.resetPosition(pose, getHeading());
+    }
+
+    public void resetPose() {
+        odometry.resetPosition(new Pose2d(), getHeading());
+    }
+
+    public static class AutoUtil {
+
+        public static final double maxAccelerationMetersPerSecondSquared = 3;
+        public static final double maxAngularSpeedRadiansPerSecond = Math.PI;
+        public static final double maxAngularSpeedRadiansPerSecondSquared = Math.PI;
+
+        private static final PIDController xController = new PIDController(1, 0, 0);
+        private static final PIDController yController = new PIDController(1, 0, 0);
+
+        public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
+                maxAngularSpeedRadiansPerSecond, maxAngularSpeedRadiansPerSecondSquared);
+
+        private static final ProfiledPIDController thetaController = new ProfiledPIDController(
+                1, 0, 0, kThetaControllerConstraints);
+
+        private WpiDriveSubsystem driveSubsystem;
+
+        public AutoUtil(WpiDriveSubsystem driveSubsystem) {
+            this.driveSubsystem = driveSubsystem;
+            thetaController.enableContinuousInput(-Math.PI, Math.PI);
+        }
+
+        public SwerveControllerCommand generateTrajectoryCommand(Trajectory trajectory) {
+            driveSubsystem.setPose(trajectory.getInitialPose());
+            return new SwerveControllerCommand(trajectory, driveSubsystem::getPose, kinematics, xController,
+                    yController, thetaController, driveSubsystem::updateModules, driveSubsystem);
+        }
+
+    }
 }
