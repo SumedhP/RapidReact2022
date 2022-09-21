8f2e59a8a1985f079f4c6476b64b1a2db9534926
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-09-21 15:12:43 +0530

    Stuff

--- src/main/java/frc/team2412/robot/Subsystems/Drivetrain.java
+++ src/main/java/frc/team2412/robot/Subsystems/Drivetrain.java
@@ -1,14 +1,19 @@
 package frc.team2412.robot.Subsystems;
 
+import static frc.team2412.robot.Hardware.*;
 import com.swervedrivespecialties.swervelib.SwerveModule;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Drivetrain extends SubsystemBase{
+public class Drivetrain extends SubsystemBase {
 
 	SwerveModule[] modules;
 
-	public Drivetrain(){
+	public Drivetrain() {
+
+		modules = new SwerveModule[] { FRONT_LEFT_CONFIG.create(),
+				FRONT_RIGHT_CONFIG.create(),
+				BACK_LEFT_CONFIG.create(),
+				BACK_RIGHT_CONFIG.create()};
 
 	}
 
